157577b6698d011f290ab847b52ecbbead8b820a
Calvin Shin <calvin.shin@gmail.com>
2019-04-01 20:48:28 -0700

    created a shell of the game and improved the objects. Need to scrap the current design dynamically create objects that are then pushed into the div id=gems

--- assets/js/js.js
+++ assets/js/js.js
@@ -8,21 +8,53 @@ var game = {
         value : 0,
     },
     crystalTwo : {},
-    crystalThree : this.newCrystal,
+    crystalThree : {},
     crystalFour : {},
+    crystalSum : 0,
+    magicNumber : 0,
     // arrays that are helpful for the game
-
+    crystalValueArray : {},
+    gameSize : [1, 2, 3, 4, 5, 6],
+    // Game isvariables
+    isGameOn : false,
     // functions required for the game
-    initialize : function() {
-
+    gameStart : function() {
+        game.crystalValueArray = [7, 8, 9, 10, 11, 12];
+        game.newCrystal(game.crystalOne);
+        game.newCrystal(game.crystalTwo);
+        game.newCrystal(game.crystalThree);
+        game.newCrystal(game.crystalFour);
+        game.magicNumber = Math.round(Math.random() * game.gameSize.length)*game.crystalOne.value +
+            Math.round(Math.random() * game.gameSize.length)*game.crystalTwo.value +
+            Math.round(Math.random() * game.gameSize.length)*game.crystalThree.value +
+            Math.round(Math.random() * game.gameSize.length)*game.crystalFour.value
+        game.isGameOn = true;
+        console.log("Game has been started!")
+        console.log(game);
     },
-    newCrystal : function() {
-        this.color = "";
-        this.value = 0;
+    newCrystal : function(crystal) {
+        crystal.color = "";
+        var arrayValue = Math.floor(Math.random() * game.crystalValueArray.length)
+        crystal.value = game.crystalValueArray[arrayValue];
+        game.crystalValueArray.splice(arrayValue, 1);
     },
+// The this set the game's values.
+    // testFunction : function() {
+    //     this.color = "";
+    //     this.value = 0;
+    // },
+    // color : "red",
 }
 
-Object.assign(game.crystalTwo, game.crystalOne);
-
-console.log(game.crystalOne);
 console.log(game);
+
+$(".gem").click(function() {
+    if(game.isGameOn === true) {
+        game.crystalSum += this.value;
+        console.log(this.value);
+    }
+})
+
+$("#shopkeeper").click(function() {
+    game.gameStart();
+})
